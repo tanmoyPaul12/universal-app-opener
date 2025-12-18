40d18bf41a0f00c06d617479289b31e31fea27e0
pshycodr <115141578+pshycodr@users.noreply.github.com>
2025-12-19 01:50:24 +0530

    refactor(core): add handlers barrel file for clean imports

---
+++ packages/core/src/handlers/index.ts
@@ -0,0 +1,9 @@
+import { linkedinHandler } from "./linkedin.handler";
+import { unknownHandler } from "./unknown.handler";
+import { youtubeHandler } from "./youtube.handler";
+
+export {
+    linkedinHandler,
+    youtubeHandler,
+    unknownHandler
+}
